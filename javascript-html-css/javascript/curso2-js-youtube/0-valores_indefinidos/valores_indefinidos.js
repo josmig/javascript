a07f5106f35295a0e57140c1aa755e4eadd5cfdd
josmig <pantigosomiguel59@gmail.com>
2021-05-18 18:10:23 -0500

    valores nulos , indefinidos etc

---
+++ javascript-html-css/javascript/curso2-js-youtube/0-valores_indefinidos/valores_indefinidos.js
@@ -0,0 +1,14 @@
+
+/**
+ * Undifined indica que no se ha inicializado una variable y que el valor esta ausente
+*/
+let indefinido;
+console.log(indefinido);
+
+/**Null es un valor especial que indical la ausencia de un valor */
+let nulo = null;
+console.log(nulo);
+
+/**NaN - Not a Number */
+let noEsNumero = 'Hola'  * 3.7;
+console.log(noEsNumero);
