f226b531c6df93478bb74e0bbad67fca034262e5
josmig <pantigosomiguel59@gmail.com>
2021-05-28 23:41:22 -0500

    Comenzando con Typescript

---
+++ javascript-html-css/javascript/curso2-js-youtube/4-Tipos_operadores/basico.js
@@ -0,0 +1,16 @@
+/**
+ *  Tipos de operadores :
+ *  
+ * Uso del =
+ * = 1 igual es asignacion de variable
+ * == 2 iguales es comparacion de valores
+ * === 3 iguales es comparacion de tipo de dato y valor
+ *
+ */
+
+console.log(7==7); //True
+console.log("7" == 7); // True (compara los valores )
+console.log(0 == false); // True
+
+console.log(7 === 7); // True
+console.log("7" === 7); //False (compara tipo de datos y valores)
