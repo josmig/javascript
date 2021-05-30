c1ae023e7ced1405c621b59cdc1349b69c394137
josmig <pantigosomiguel59@gmail.com>
2021-05-30 01:03:30 -0500

    iniciando con typescript

---
+++ typescript/aprendiendo_tsc/hello_world.js
@@ -0,0 +1,5 @@
+function saludar(nombre) {
+    return "hola" + nombre;
+}
+var mi_nombre = 'Jose Miguel';
+var resultado = saludar(mi_nombre);
---
+++ typescript/aprendiendo_tsc/hello_world.ts
@@ -0,0 +1,8 @@
+function saludar(nombre){
+    return "hola" + nombre;
+}
+let mi_nombre = 'Jose Miguel';
+let resultado = saludar(mi_nombre);
+
+console.log('Hola')
+
