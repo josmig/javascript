2cc32e61473b7cb75c5006511cc29d6cc2512f02
josmig <pantigosomiguel59@gmail.com>
2021-06-01 10:25:17 -0500

    tipos_operadores

--- javascript-html-css/javascript/curso_2/4-Tipos_operadores/basico.js
+++ javascript-html-css/javascript/curso_2/4-Tipos_operadores/basico.js
@@ -14,3 +14,29 @@ console.log(0 == false); // True
 
 console.log(7 === 7); // True
 console.log("7" === 7); //False (compara tipo de datos y valores)
+
+console.log(0 === false); // como sabemos compara valor como tipo de dato
+
+//Incremento - decremento
+let i = 1;
+//i = i+2;
+//i +=3;
+i*=3;
+console.log(i)
+//Operador unario
+/*
+* i++
+* i--
+* ++i
+* --i
+* */
+let a = 7;
+++a;
+console.log(a)
+
+/*Operadores logicos*/
+/**
+  || or -> Se ejecuta siempre y cuando una de las "condiciones" se cumpla
+  && And -> Se ejecuta siempre y cuando "todas las condiciones" se cumplan
+ */
+
