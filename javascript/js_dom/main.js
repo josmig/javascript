ea4aab7ebb27c603477c0a7b78d5a5cf7d76b733
josmig <pantigosomiguel59@gmail.com>
2021-07-05 00:25:28 -0500

    to-do-list-1

---
+++ javascript/js_dom/main.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @form  1-Captura el elemento (formulario)por JS
+ *        2-Se le asigna un evento(submit)
+ * @task  1-Captura el elemento (input) por JS
+ * @textoTask 1- Captura el valor del elemento Task(input) se lo que hayamos escrito y reemplazamos su valor a vacio luego de que se termine el evento submit
+ * 
+ */
+const todos = [];
+const form = document.getElementById('form');
+form.onsubmit = (e) => {
+    e.preventDefault(); // con esto no se actualiza la pagina al momento de enviar la tarea actual del input
+    const task = document.getElementById('task');
+    const textoTask = task.value;
+    task.value ='';
+    todos.push(textoTask);
+    /* console.log(textoTask); */
+
+    const lista = document.getElementById('lista');
+    lista.innerHTML = '';
+    for(let i=0; i < todos.length; i++){
+        lista.innerHTML += '<li>'+ todos[i] + '</li>';
+    }   
+}
