3491aefdc46c071c379c66439ad27caa0b798a14
josmig <pantigosomiguel59@gmail.com>
2021-07-05 23:24:26 -0500

    to-do-list + localstorage

--- javascript/js_dom/main.js
+++ javascript/js_dom/main.js
@@ -1,4 +1,4 @@
-/**
+/**s
  * 
  * @form  1-Captura el elemento (formulario)por JS
  *        2-Se le asigna un evento(submit)
@@ -6,16 +6,13 @@
  * @textoTask 1- Captura el valor del elemento Task(input) se lo que hayamos escrito y reemplazamos su valor a vacio luego de que se termine el evento submit
  * 
  */
-const todos = [];
-const form = document.getElementById('form');
-form.onsubmit = (e) => {
-    e.preventDefault(); // con esto no se actualiza la pagina al momento de enviar la tarea actual del input
-    const task = document.getElementById('task');
-    const textoTask = task.value;
-    task.value ='';
-    todos.push(textoTask);
-    /* console.log(textoTask); */
 
+/* const todos = []; */
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
+const form = document.getElementById('form');
+
+
+const render = () => {
     const lista = document.getElementById('lista');
     /* lista.innerHTML = '';
     for(let i=0; i < todos.length; i++){
@@ -28,4 +25,32 @@ form.onsubmit = (e) => {
     lista.innerHTML = listaTemplate.join('');
     console.log(listaTemplate);
 
+    //.splice() -> nos permite eliminar valores de un array 1valor (posición del elemento a eliminar) , 2valor(cuantos valores se eliminara)
+    const elementos = document.querySelectorAll('#lista li');
+    elementos.forEach ((ele, i)=> {
+        ele.addEventListener('click', ()=> {
+            //Porquequeremos ver los nodos? Los nodos padres tienen la capacidad de eliminar a sus hijos
+            ele.parentNode.removeChild(ele);
+            todos.splice(i,1); //borrando del array (indice, numero de elementos a borrar)
+            console.log(todos, i);
+            actulizaTodos(todos);
+            render(); //llamando a la funcion para que se actualize
+        });
+    });
+}
+const actulizaTodos = todos => {
+    const  todoString = JSON.stringify(todos)
+    localStorage.setItem('todos', todoString);
+}
+render();
+form.onsubmit = (e) => {
+    e.preventDefault(); // con esto no se actualiza la pagina al momento de enviar la tarea actual del input
+    const task = document.getElementById('task');
+    const textoTask = task.value;
+    task.value ='';
+    todos.push(textoTask);
+    /* console.log(textoTask); */   
+    //Guardando en el TODOLIST
+    actulizaTodos(todos);
+    render();
 }
