e44b39e31d57246cf9380db36aa584ddbaacb91c
josmig <pantigosomiguel59@gmail.com>
2021-07-20 22:57:37 -0500

    destructuracion de argumentos

---
+++ typescript/aprendiendo_tsc/intro_angular/5-Destructuring_arguments.ts
@@ -0,0 +1,31 @@
+
+
+interface Producto{
+  desc: string;
+  precio: number;
+}
+
+const telefono: Producto = {
+  desc: 'Alcatel',
+  precio: 150
+}
+const telefono2: Producto ={
+  desc: 'Motorola',
+  precio: 200
+}
+
+
+function calculaISV( productos: Producto[]): [number,number]{
+  let total = 0;
+  productos.forEach(({precio}) => {
+    total += precio;
+  });
+  return [total,total * 0.15];
+}
+
+
+const articulos = [telefono, telefono2];
+const [total,isv] = calculaISV(articulos);
+
+console.log('Total : ' + total);
+console.log('ISV :' , isv);
